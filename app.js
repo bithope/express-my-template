3629081e83baad1b222dfd14612c9758a9b26ac8
Thai Bithope <124513378+bithope@users.noreply.github.com>
2023-02-26 15:11:31 +0000

    First Templates

--- app.js
+++ app.js
@@ -4,14 +4,45 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
- var today = new Date();
- var currentday = today.getday();
- console.log(currentday);
-})
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thusrday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+
+    default:
+        console.log("Error!!");
+
+  }
+  res.render("list", {
+    kindOfday: day
+    });
+});
 
 
 
-app.listen(3000, function(){
-    console.log("runnins!");
-})
+app.listen(3000, function () {
+  console.log("running!");
+});
