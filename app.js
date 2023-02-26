4848a3b30e827dafca8c5938061ced9b66939c77
Thai Bithope <124513378+bithope@users.noreply.github.com>
2023-02-26 13:51:45 +0000

    test

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
 
+app.get("/", function(req, res){
+ res.send("konglfie")
+})
+
+
+
+app.listen(3000, function(){
+    console.log("runnins!");
+})
